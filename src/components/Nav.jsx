93fbc2a559dfd2b2c91670c162c8b24da72ff5c6
evan8383 <evanbennett83@gmai.com>
2024-02-13 16:39:47 -0500

    added reactivity to nav element

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,35 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between bg-gray-800 p-4">
-      <ul className="flex space-x-4">
-        <li>
-          <Link to="/" className={`text-white ${location.pathname === '/' ? 'font-bold' : ''}`}>Home</Link>
-        </li>
-        <li>
-          <Link to="/about" className={`text-white ${location.pathname === '/about' ? 'font-bold' : ''}`}>About</Link>
-        </li>
-        <li>
-          <Link to="/portfolio" className={`text-white ${location.pathname === '/portfolio' ? 'font-bold' : ''}`}>Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact" className={`text-white ${location.pathname === '/contact' ? 'font-bold' : ''}`}>Contact</Link>
-        </li>
-        <li>
-          <Link to="/resume" className={`text-white ${location.pathname === '/resume' ? 'font-bold' : ''}`}>Resume</Link>
-        </li>
-      </ul>
-    </nav>
-
+    <div className="xs:h-fit h-screen sm:flex sm:flex-col justify-between ">
+      <nav className="bg-gray-800 p-4 sm:max-w-xs flex-grow">
+        <h1 className="text-2xl text-white font-extrabold mb-8">Evan Bennett</h1>
+        <button onClick={() => setIsOpen(!isOpen)} className="sm:hidden block text-white">
+          <i className="fa fa-bars"></i>
+        </button>
+        <ul className={`flex flex-col space-y-4 ${isOpen ? 'block' : 'hidden'} sm:block`}>
+          <li>
+            <Link to="/" className={`text-white ${location.pathname === '/' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className={`text-white ${location.pathname === '/about' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio" className={`text-white ${location.pathname === '/portfolio' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact" className={`text-white ${location.pathname === '/contact' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>Contact</Link>
+          </li>
+          <li>
+            <Link to="/resume" className={`text-white ${location.pathname === '/resume' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>Resume</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
