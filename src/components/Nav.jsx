d7337b6350f42b42bf0e22423ed3d3f234f1ed54
evan8383 <evanbennett83@gmai.com>
2024-02-13 18:34:39 -0500

    small changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,12 +8,12 @@ function Nav() {
   return (
     <>
       <div className="sm:flex sm:flex-col justify-between ">
-        <nav className="bg-gray-800 p-4 sm:max-w-fit flex-grow sm:rounded-r">
-          <h1 className="text-2xl text-white font-extrabold mb-8">Evan Bennett</h1>
+        <nav className="max-sm:flex flex-wrap max-sm: justify-between bg-gray-800 p-4 sm:max-w-fit flex-grow sm:rounded-r">
+          <h1 className="text-2xl text-white font-extrabold ">Evan Bennett</h1>
           <button onClick={() => setIsOpen(!isOpen)} className="sm:hidden block text-white">
             <i className="fa fa-bars"></i>
           </button>
-          <ul className={`flex flex-col space-y-4 ${isOpen ? 'block' : 'hidden'} sm:block`}>
+          <ul className={`flex max-sm:right-0 max-sm:top-10 max-sm:p-14 max-sm:rounded-lg max-sm:absolute bg-gray-800 max-sm:text-center text-left mt-4 flex-col flex-wrap space-y-4 ${isOpen ? 'block' : 'hidden'} sm:block w-fit`}>
             <li>
               <Link to="/" className={`text-white ${location.pathname === '/' ? 'font-bold' : ''} hover:text-blue-400 transition duration-300`}>Home</Link>
             </li>
