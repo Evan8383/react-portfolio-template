ff8bde1cebaf70eda855c8192bbe25ee07068b4a
evan8383 <evanbennett83@gmai.com>
2024-02-17 12:08:50 -0500

    functional state with all pages but resume

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,8 +4,9 @@ export default function Project(props) {
     <div className="max-w-sm md:w-1/3 lg:1/2 w-full rounded-lg border shadow-md overflow-hidden">
       <div className="relative overflow-hidden group">
         <img
+          onClick={() => window.open(props.link)}
           alt={props.title}
-          className="object-cover w-full h-48 transition-transform group-hover:scale-105"
+          className="object-cover w-full h-48 transition-transform group-hover:scale-105 cursor-pointer"
           height={200}
           src={props.image}
           style={{
